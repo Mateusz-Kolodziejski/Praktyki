2cd141f84524fec85376f55579a0de712fdd4903
jaca <vitouejj@gmail.com>
2020-11-16 21:21:38 +0100

    refacotr Menu

--- RetardDungeon/src/Main.java
+++ RetardDungeon/src/Main.java
@@ -3,17 +3,15 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) throws InterruptedException {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Witaj w Retard Dungeon! \nTwoim zadaniem będzie walka z przeciwnikami oraz ulepszanie swojego bohatera");
-        System.out.println("Wpisz imię swojej postaci :");
-        Player.name = scan.nextLine();
-        System.out.println("A więc, "+Player.name+" czas rozpocząć twoją przygodę!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Witaj w Retard Dungeon!");
+        System.out.println("Twoim zadaniem będzie walka z przeciwnikami oraz ulepszanie swojego bohatera");
+        System.out.println("Wpisz imię swojej postaci: ");
+        var player = new Player(scanner.nextLine());
+        System.out.println(player.getName() + " czas rozpocząć twoją przygodę!");
         Thread.sleep(2000);
         System.out.println("\n Próbujesz dotrzeć do miasteczka jednak aby tego dokonać musisz pokonać stojących na twojej drodze przeciwników");
-
-        Menu menu;
-        menu = new Menu();
+        Menu menu = new Menu(scanner);
         menu.game_menu();
 
     }
--- RetardDungeon/src/Menu.java
+++ RetardDungeon/src/Menu.java
@@ -1,28 +1,35 @@
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class Menu
-{
-   public void game_menu()
-    {
-        Scanner scan = new Scanner(System.in);
+public class Menu {
+    private final Scanner scanner;
 
+    public Menu(Scanner scanner) {
+        this.scanner = Objects.requireNonNull(scanner);
+    }
+
+    public void game_menu() {
         System.out.println("Witaj w miasteczku! Co chciałbyś zrobić?");
-        System.out.println("\t 1) Wejdź do dungeonu 1 poziomu \n \t 2) Odwiedź sklep");
-        String input = scan.nextLine();
-
-        switch (input)
-        {
-            case "1" ->
-                    {
-                        System.out.println("Schodzisz po schodach w głąb ciemnego korytarza");
-
-                    }
-            case "2" -> System.out.println("Witaj w Retard Shop!");
+        System.out.println("\t 1) Wejdź do dungeonu 1 poziomu");
+        System.out.println("\t 2) Odwiedź sklep");
+        System.out.println("\t 3) Bitwa");
+        String input = scanner.nextLine();
+        switch (input) {
+            case "1":
+                System.out.println("Schodzisz po schodach w głąb ciemnego korytarza");
+            case "2":
+                System.out.println("Witaj w Retard Shop!");
+            case "3":
+                var battle = new Battle(createRandomEnemy());
+            default:
+                game_menu();
         }
     }
 
-
-
-
-
+    private Enemy createRandomEnemy() {
+        return new Enemy("jaca",
+                ThreadLocalRandom.current().nextInt(1, 100),
+                ThreadLocalRandom.current().nextInt(1, 100));
+    }
 }
